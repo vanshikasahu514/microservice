2daeb9563a70e7a9d50368d14441c0b1bb4268e4
Vanshika Sahu <vanshikasahu514@gmail.com>
2026-05-25 23:48:47 -0400

    Fix whitenoise config

--- product_service/config/config/settings.py
+++ product_service/config/config/settings.py
@@ -160,4 +160,3 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- user_service/config/config/settings.py
+++ user_service/config/config/settings.py
@@ -154,7 +154,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
